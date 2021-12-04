6592644fe990b96789b9a13be8814b3d27df43c3
druchiat <62007520+druchiat@users.noreply.github.com>
2021-12-04 21:00:25 +0700

    [LAYOUT] activity_login.xml

--- app/src/main/java/com/example/bukupr3/CreateBukuPrActivity.kt
+++ app/src/main/java/com/example/bukupr3/CreateBukuPrActivity.kt
@@ -3,9 +3,9 @@ package com.example.bukupr3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CreateBukuPrActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_create_buku_pr)
     }
 }
---
+++ app/src/main/java/com/example/bukupr3/DetailTugasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bukupr3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailTugasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_tugas)
+    }
+}
