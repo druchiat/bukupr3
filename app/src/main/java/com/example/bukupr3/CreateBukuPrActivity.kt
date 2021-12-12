aa6b5e090d4501400c61d1b8346c6ce07b784c46
druchiat <62007520+druchiat@users.noreply.github.com>
2021-12-12 12:14:13 +0700

    [PROGRESS] CreateBukuPrActivity.kt

--- app/src/main/java/com/example/bukupr3/CreateBukuPrActivity.kt
+++ app/src/main/java/com/example/bukupr3/CreateBukuPrActivity.kt
@@ -1,10 +1,13 @@
 package com.example.bukupr3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.button.MaterialButton
 import com.google.firebase.firestore.FirebaseFirestore
 import org.w3c.dom.Text
 
@@ -16,15 +19,34 @@ class CreateBukuPrActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_buku_pr)
 
-
+        val mbBackCBP : MaterialButton = findViewById(R.id.mbBackCBP)
+        val mbDoneCBP : MaterialButton = findViewById(R.id.mbDoneCBP)
         val etNamaBukuPr : EditText = findViewById(R.id.etNamaBukuPr)
 
         db = FirebaseFirestore.getInstance()
 
+        mbBackCBP.setOnClickListener {
+            startActivity(Intent(this,BukuPrActivity::class.java))
+            finish()
+        }
 
+        mbDoneCBP.setOnClickListener {
+            val nama = etNamaBukuPr.text.toString()
+            if (TextUtils.isEmpty(nama)){
+                etNamaBukuPr.error = "Enter name here"
+                return@setOnClickListener
+            }
+            if (nama.length > 20) {
+                etNamaBukuPr.error = "Maximum of 20 characters"
+                return@setOnClickListener
+            }
+            addBukuPr(nama)
+        }
 
     }
 
+    private fun addBukuPr(nama : String){
 
+    }
 
 }
