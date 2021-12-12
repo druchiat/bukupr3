8e6a413fef3cf1db5f213e5a1cd7aaadbbe5e094
druchiat <62007520+druchiat@users.noreply.github.com>
2021-12-12 19:52:37 +0700

    [PROGRESS] CreateBukuPrActivity.kt

--- app/src/main/java/com/example/bukupr3/CreateBukuPrActivity.kt
+++ app/src/main/java/com/example/bukupr3/CreateBukuPrActivity.kt
@@ -7,8 +7,14 @@ import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.example.bukupr3.model.BukuPr
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
+import com.google.firebase.ktx.Firebase
 import org.w3c.dom.Text
 
 class CreateBukuPrActivity : AppCompatActivity() {
@@ -40,12 +46,23 @@ class CreateBukuPrActivity : AppCompatActivity() {
                 etNamaBukuPr.error = "Maximum of 20 characters"
                 return@setOnClickListener
             }
-            addBukuPr(nama)
+            val currentUser = Firebase.auth.currentUser?.uid.toString()
+            val bukupr = BukuPr(currentUser, nama)
+
+            addBukuPr(bukupr)
         }
 
     }
 
-    private fun addBukuPr(nama : String){
+    private fun addBukuPr(bukupr : BukuPr){
+        db.collection("BukuPr").add(bukupr)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+                startActivity(Intent(this, BukuPrActivity::class.java))
+                finish()
+            } .addOnFailureListener {
+                Toast.makeText(this, "Failed. Please try again.", Toast.LENGTH_LONG).show()
+            }
 
     }
 
--- app/src/main/java/com/example/bukupr3/model/BukuPr.kt
+++ app/src/main/java/com/example/bukupr3/model/BukuPr.kt
@@ -1,7 +1,6 @@
 package com.example.bukupr3.model
 
 data class BukuPr(
-    var idBukuPr : String,
     var uid : String,
     var namaBukuPr : String
 )
